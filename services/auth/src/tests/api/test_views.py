c25f1a926c868b412967b611e6cfbbd6a91fa041
DimaK <korbanitskiy@gmail.com>
2023-10-02 21:39:17 +0300

    add tests

--- services/auth/src/tests/api/test_views.py
+++ services/auth/src/tests/api/test_views.py
@@ -25,3 +25,17 @@ async def test_login_existing_user(http_client):
 
     assert response.status_code == 200, response.text
     assert response.json() == {"access_token": ANY, "token_type": "bearer"}
+
+
+async def test_access_by_token(http_client):
+    user = await UserFactory.create()
+    form_data = {"username": user.name, "password": "password"}
+    login_response = await http_client.post("/login", data=form_data)
+    login_respone_json = login_response.json()
+
+    user_response = await http_client.get(
+        "/users/me",
+        headers={}
+    )
+
+    assert user_response.status_code == 200
